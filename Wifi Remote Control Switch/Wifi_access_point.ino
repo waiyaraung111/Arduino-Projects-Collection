63cdb1a437992a77ff6d9e8b9672b52672bec8c8
Wai Yar Aung <waiyaraung111@gmail.com>
2024-10-15 21:03:54 +0700

    Add a new file.

---
+++ Wifi Remote Control Switch/Wifi_access_point.ino
@@ -0,0 +1,71 @@
+#define REMOTEXY_MODE__ESP8266_HARDSERIAL_POINT
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_SERIAL Serial
+#define REMOTEXY_SERIAL_SPEED 115200
+#define REMOTEXY_WIFI_SSID "RemoteXY"
+#define REMOTEXY_WIFI_PASSWORD "12345678"
+#define REMOTEXY_SERVER_PORT 6377
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =   // 111 bytes
+  { 255,3,0,0,0,104,0,16,8,1,2,0,20,14,22,11,94,26,31,31,
+  79,78,0,79,70,70,0,2,0,20,41,22,11,135,26,31,31,79,78,0,
+  79,70,70,0,2,0,19,73,22,11,1,26,31,31,79,78,0,79,70,70,
+  0,129,0,19,65,24,6,1,76,69,68,32,82,101,100,0,129,0,20,33,
+  30,6,136,76,69,68,32,71,114,101,101,110,0,129,0,21,5,18,6,94,
+  76,69,68,32,89,101,108,108,111,119,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  uint8_t switch_1; // =1 if switch ON and =0 if OFF 
+  uint8_t switch_2; // =1 if switch ON and =0 if OFF 
+  uint8_t switch_3; // =1 if switch ON and =0 if OFF 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+#define PIN_SWITCH_1 8
+#define PIN_SWITCH_2 9
+#define PIN_SWITCH_3 10
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  pinMode (PIN_SWITCH_1, OUTPUT);
+  pinMode (PIN_SWITCH_2, OUTPUT);
+  pinMode (PIN_SWITCH_3, OUTPUT);
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  digitalWrite(PIN_SWITCH_1, (RemoteXY.switch_1==0)?LOW:HIGH);
+  digitalWrite(PIN_SWITCH_2, (RemoteXY.switch_2==0)?LOW:HIGH);
+  digitalWrite(PIN_SWITCH_3, (RemoteXY.switch_3==0)?LOW:HIGH);
+  
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay() 
+
+
+}
